3b4e54a192a0c3ab8fefaf9787d1ae0079a40c97
jth1209 <xkgshand@naver.com>
2024-07-16 12:50:14 +0900

    inner class final

---
+++ Java0716/src/Java0716/Outter.java
@@ -0,0 +1,25 @@
+package Java0716;
+
+public class Outter {
+//	public void method1(final int age) {
+//		final int local = 1;
+//		age = 100;//final형으로 선언된 int 매개변수의 값을 이너 클래스에 변경해서 사용할 수 없음
+//		local = 100;
+//		class inner{
+//			public void method(int age) {
+//				int result = age+local;
+//			}
+//		}
+//	}
+	
+//	public void method1(int age) {
+//		int local = 1;
+//		age = 100;
+//		local = 100;
+//		class inner{
+//			public void method(int age) {
+//				int result = age+local;//그냥 int형으로 선언을 하더라도 final처럼 인식함.
+//			}
+//		}
+//	}
+}
